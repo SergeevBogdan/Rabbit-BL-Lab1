f8077dbada3409c42a200298a8570be463128921
Bogdan <sergeevb141@gmail.com>
2025-12-04 16:55:30 +0700

    аа58 - версия 5.1

---
+++ ViewManager/IViewManagerService.cs
@@ -0,0 +1,16 @@
+﻿using RabbitViewModels;
+using System;
+
+namespace ViewManager
+{
+    public interface IViewManagerService
+    {
+        void RegisterView(Type viewType, BaseViewModel viewModel);
+        void ShowView(Type viewType);
+        void CloseView(Type viewType);
+        BaseViewModel GetViewModel(Type viewType);
+
+        event Action<Type, BaseViewModel> ViewRequested;
+        event Action<Type> ViewClosed;
+    }
+}
